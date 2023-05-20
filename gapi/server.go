d4362d70710273a072f18c8bd078f6ab9dc7d4ac
Nguyen Duong <nguyen.duong@smoothstack.com>
2023-05-20 00:58:24 -0700

    adding worker and redis

--- gapi/server.go
+++ gapi/server.go
@@ -7,29 +7,30 @@ import (
 	"github.com/Huy1996/simplebank/pb"
 	"github.com/Huy1996/simplebank/token"
 	"github.com/Huy1996/simplebank/util"
+	"github.com/Huy1996/simplebank/worker"
 )
 
 // Server serves gRPC requests for our banking service.
 type Server struct {
 	pb.UnimplementedSimpleBankServer
-	config 		util.Config
-	store 		db.Store
-	tokenMaker 	token.Maker
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+	func NewServer(config util.Config, store db.Store, taskDistributor worker.TaskDistributor) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker: %w", err)
 	}
 	server := &Server{
-		config:		config,
-		store: 		store,
+		config:     config,
+		store:      store,
 		tokenMaker: tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	return server, nil
 }
-
-
